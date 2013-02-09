5d7158725ac1f1050ec9964c97949a7d0164b48a
Alexander Makaev <alexrule7@gmail.com>
2013-02-10 01:26:10 +0400

    [+] Icons

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "javascripts"
+javascripts_dir = "js"
 
 output_style = :compressed
 line_comments = false
