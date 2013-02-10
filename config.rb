e2787e1356b4f339d223e06477aa3da0953c778d
Alexander Makaev <alexrule7@gmail.com>
2013-02-10 05:07:11 +0400

    no message

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+http_path = ""
+css_dir = "static/stylesheets"
+sass_dir = "static/sass"
+images_dir = "static/images"
+javascripts_dir = "static/js"
 
 output_style = :compressed
 line_comments = false
